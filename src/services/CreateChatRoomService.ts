f22dc47eafed16b2de4cc0b3a878d75b772dadb6
Iago Alexandre <iagoaap@outlook.com>
2023-04-03 06:04:37 -0300

    list all users

---
+++ src/services/CreateChatRoomService.ts
@@ -0,0 +1,8 @@
+import { injectable } from "tsyringe";
+
+@injectable()
+export class CreateChatRoomService {
+  async execute(idUsers: String[]) {
+
+  }
+}
--- src/websocket/ChatService.ts
+++ src/websocket/ChatService.ts
@@ -1,19 +1,27 @@
 import { container } from "tsyringe"
 import { io } from "../http"
 import { CreateUserService } from "../services/CreateUserService"
+import { GetAllUsersService } from "../services/GetAllUsersService"
 
 io.on("connect", socket => {
   socket.on("start", async (data) => {
     const { name, email, avatar } = data;
-    const createUserService = container.resolve(CreateUserService)
+    const createUserService = container.resolve(CreateUserService);
 
     const user = await createUserService.execute({
       email, 
       avatar, 
       name, 
       socket_id: socket.id
-    })
+    });
 
-    console.log(user)
+    socket.broadcast.emit("new_users", user);
+  })
+
+  socket.on("get_users", async (callback) => {
+    const getAllUsersService = container.resolve(GetAllUsersService);
+    const users = await getAllUsersService.execute();
+
+    callback(users)
   })
 })
